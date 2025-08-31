da955626ef6a0d68dd447f14aabb4083a2fe28fe
Hiteshkumar24 <Hiteshkumar240520040@gmail.com>
2025-08-31 18:13:01 +0530

    commit24

--- Index.js
+++ Index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app=express();
 
 const port = process.env.PORT || 8080;
-app.listen(PORT, () => {
+app.listen(port, () => {
     console.log(`server is running on port ${PORT}`);
 });
 
